bc81cc058deb58feb6b1b2986276b7c9ee3a54cf
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-03 12:34:13 +0900

    ハンバーガー修正

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,6 @@ $(".js-header-hamburger").click(function () {
     $(".sp-nav").toggleClass("js-open");
     $(".js-header-hamburger>span").toggleClass("js-open");
     $(".js-header-hamburger").toggleClass("js-open");
-
-      //クリックしたときMV以上だったら、透過しない
-    // if ($('.js-mainview').height() < $(window).scrollTop()) {
-    //   $(".js-header-hamburger>span").addClass("js-opacity");
-    // } else { //クリックしたときMV以下だったら、透過する
-    //   $(".js-header-hamburger>span").toggleClass("js-opacity");
-    // }
   });
 
 });
