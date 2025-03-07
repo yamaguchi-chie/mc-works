cb63d091b2e4de051c38ffb0cb5f3aefe54c487e
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-07 13:02:51 +0900

    施工実績詳細PC

--- page-contact.php
+++ page-contact.php
@@ -4,68 +4,9 @@
         <div class="sub-contact__title">
             <h1 class="page-title txt"><span>contact</span>お問い合わせ</h1>
         </div>
-            <div class="sub-contact__body">
-            <!-- <form class="sub-contact__form" id="form"> -->
-                <?php the_content(); ?>
-                <!-- <dl class="form-rows">
-                    <div class="form-rows__item form-row">
-                        <dt class="form-row__text txt">
-                            <label for="your-name">会社名</label>
-                        </dt>
-                        <dd class="form-row__input txt">
-                            <input type="text" id="your-name" placeholder="例：株式会社OOOO">
-                        </dd>
-                    </div>
-                    <div class="form-rows__item form-row">
-                        <dt class="form-row__text txt">
-                            <label for="your-name" class="form-row__required">お名前</label>
-                        </dt>
-                        <dd class="form-row__input txt">
-                            <input type="text" id="your-name" placeholder="例：和久須　太郎" required>
-                        </dd>
-                    </div>
-                    <div class="form-rows__item form-row">
-                        <dt class="form-row__text txt">
-                            <label for="your-tel">電話番号</label>
-                        </dt>
-                        <dd class="form-row__input txt">
-                            <input type="tel" id="your-tel" placeholder="例：00-000-0000" required>
-                        </dd>
-                    </div>
-                    <div class="form-rows__item form-row">
-                        <dt class="form-row__text txt">
-                            <label for="your-mail" class="form-row__required">メールアドレス</label>
-                        </dt>
-                        <dd class="form-row__input txt">
-                            <input type="email" id="your-mail" placeholder="例：example@.com" required>
-                        </dd>
-                    </div>
-                    <div class="form-rows__item form-row form-row--start">
-                        <dt class="form-row__text txt">
-                            <label for="your-contact" class="form-row__required">お問い合わせ内容</label>
-                        </dt>
-                        <dd class="form-row__input txt">
-                            <textarea name="your-contact" id="your-contact" required></textarea>
-                        </dd>
-                    </div>
-                </dl>
-                <div class="sub-contact__check">
-                    <label for="your-privacy">
-                        <input type="checkbox" id="your-privacy" name="" value="プライバシーポリシーの同意" required>
-                        <p class="sub-contact__link">
-                            <a href="">プライバシーポリシー</a>に同意します。
-                        </p>
-                    </label>
-                </div>
-                <div class="sub-contact__submit">
-                    <input class="btn btn--submit" type="submit" value="送信" id="js-submit" disabled>
-                    <span></span>
-                </div> -->
-            <!-- </form> -->
+        <div class="sub-contact__body">
+            <?php the_content(); ?>
         </div>
     </div>
 </section>
-
-
-
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,66 +1,90 @@
 <?php get_header(); ?>
-<?php
-$post_type = get_post_type();
-$post_type_data = get_post_type_object($post_type);
-$post_type_label = $post_type_data->labels->name;
-?>
-<main>
-  <section class="p-post-article">
-    <div class="p-post-article__inner l-inner">
-      <h1 class="p-post-article__title"><?php echo $post_type_label; ?></h1>
-      <!-- パンくずリスト -->
-      <?php if (function_exists('bcn_display')) : ?>
-        <div class="p-post-article__breadcrumb c-breadcrumb" typeof="BreadcrumbList" vocab="http://schema.org/">
-          <?php bcn_display(); ?>
+<section class="detail-works">
+    <div class="detail-works__inner inner">
+        <div class="detail-works__title">
+            <h1 class="page-title txt"><span>works</span>会社案内</h1>
         </div>
-      <?php endif; ?>
-
-
-      <div class="p-post-article__wrap">
-        <?php if (have_posts()) : ?>
-          <?php while (have_posts()) : the_post(); ?>
-            <!-- サムネイル表示 -->
-            <?php if (has_post_thumbnail()) : ?>
-              <figure class="p-post-article__main-img">
-                <?php the_post_thumbnail('large'); ?>
-              </figure>
-            <?php endif; ?>
-
-            <div class="p-post-article__content">
-              <div class="p-post-article__content-inner">
-                <div class="p-post-article__content-wrap">
-                  <div class="p-post-article__info">
-                    <?php
-                    $categories = get_the_category();
-                    if (!empty($categories)) :
-                    ?>
-                      <ul class="p-post-article__category">
-                        <?php foreach ($categories as $category) : ?>
-                          <li><?php echo esc_html($category->name); ?></li>
-                        <?php endforeach; ?>
-                      </ul>
-                    <?php endif; ?>
-                    <time class="p-post-article__data" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y/m/d'); ?></time>
-                  </div>
-                  <div class="p-post-article__title-box">
-                    <h2 class="p-post-article__title"><?php the_title(); ?></h2>
-                  </div>
-                  <div class="p-post-article__contents">
-                    <?php the_content(); ?>
-                  </div>
-                </div>
+        <div class="detail-works__body">
+          <h2 class="detail-works__subTl"><?php the_title(); ?></h2>
+          <div class="detail-works__thumbs">
+            <?php if(has_post_thumbnail()){
+            the_post_thumbnail('full', array('alt' => get_the_title()));
+            }else{ ;?>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/no_image.jpg" alt="no image">
+            <?php } ;?>
+          </div>
+            <dl class="detail-works__label">
+              <div class="detail-works__item">
+                  <dt class="detail-works__left">
+                      <p class="txt">所在地 : </p>
+                  </dt>
+                  <dd class="detail-works__right">
+                      <p class="txt">
+                        <?php $adress = get_field('adress');
+                        if($adress): ?>
+                            <?php echo the_field('adress'); ?>
+                        <?php endif; ?>
+                      </p>
+                  </dd>
               </div>
-            </div>
-          <?php endwhile; ?>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
-
-        <!-- pager -->
-        <?php get_template_part('parts/common/p-pager-list'); ?>
+              <div class="detail-works__item">
+                  <dt class="detail-works__left">
+                      <p class="txt">主要用途 :</p>
+                  </dt>
+                  <dd class="detail-works__right">
+                      <p class="txt">
+                      <?php $main_use = get_field('main_use');
+                      if($main_use): ?>
+                          <?php echo the_field('main_use'); ?>
+                      <?php endif; ?>
+                      </p>
+                  </dd>
+              </div>
+              <div class="detail-works__item">
+                  <dt class="detail-works__left">
+                      <p class="txt">工事種別 : </p>
+                  </dt>
+                  <dd class="detail-works__right">
+                      <p class="txt">
+                      <?php $construction_type = get_field('construction_type');
+                      if($construction_type): ?>
+                          <?php echo the_field('construction_type'); ?>
+                      <?php endif; ?>
+                      </p>
+                  </dd>
+              </div>
+              <div class="detail-works__item">
+                  <dt class="detail-works__left">
+                      <p class="txt">主要構造 : </p>
+                  </dt>
+                  <dd class="detail-works__right">
+                      <p class="txt">
+                      <?php $main_structure = get_field('main_structure');
+                      if($main_structure): ?>
+                          <?php echo the_field('main_structure'); ?>
+                      <?php endif; ?>
+                      </p>
+                  </dd>
+              </div>
+              <div class="detail-works__item">
+                  <dt class="detail-works__left">
+                      <p class="txt">施工面積 : </p>
+                  </dt>
+                  <dd class="detail-works__right">
+                      <p class="txt">
+                      <?php $area = get_field('area');
+                      if($area): ?>
+                          <?php echo the_field('area'); ?>
+                      <?php endif; ?>
+                      </p>
+                  </dd>
+              </div>
+            </dl>
+        </div>
       </div>
-    </div>
-  </section>
-  <!-- 関連スタッフブログ -->
-  <?php get_template_part('parts/post/p-post-connect'); ?>
-</main>
+</section>
+
+
+
+<?php get_template_part('parts/post/contact'); ?>
 <?php get_footer(); ?>
