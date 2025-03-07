fe7521ea33dbe54afcf9512e55bc44d2fb72a697
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-07 10:53:03 +0900

    会社案内PC

--- page-company.php
+++ page-company.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
-
+<section class="company">
+    <div class="company__inner inner">
+        <div class="company__title">
+            <h1 class="page-title txt"><span>company</span>会社案内</h1>
+        </div>
+    </div>
+    <div class="company__body">
+        <h2 class="company__subTl">確かな施工があってこそ、<br>経年が味わいになる。</h2>
+        <p class="company__txt txt">建築は、完成した瞬間が終わりではない。<br>本当に価値があるのは、住みやすく、時間とともに馴染み、深みを増していく空間。<br>適材適所の素材選び、無駄のない構造、使い込むほどに美しさが際立つ仕上げ——<br>そのすべてを支えるのは、確かな施工。<br>そして、丁寧につくられた空間だからこそ、経年変化が味わいとなり、本物のヴィンテージへと育っていく。<br>見せるためではなく、暮らしの中で育つ建築を。<br>細部までこだわり抜いた設計と施工で、あなたの理想をカタチにします。。</p>
+    </div>
+</section>
 
 
 <?php get_footer(); ?>
