f8f2a03027f5ee87edea5469ccac2bb469d123b2
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-07 10:59:20 +0900

    事業内容PC

--- page-company.php
+++ page-company.php
@@ -11,5 +11,22 @@
     </div>
 </section>
 
+<section class="business">
+    <div class="business__inner inner">
+        <div class="business__title">
+            <h2 class="section-title txt"><span>business</span>事業内容</h2>
+        </div>
+        <ul class="business__items">
+            <li class="business__item">
+                <h3 class="business__subTl">住宅の建築・リフォーム</h3>
+                <p class="business__txt txt">新築住宅の設計・施工とリフォームを手がけており、理想の住まいを実現するため、デザイン性・機能性に優れた家づくりを行います。新築では一から丁寧にプランニングし、快適で長く愛せる住まいを提供。リフォームではライフスタイルやお悩みに寄り添い、暮らしやすい空間へと生まれ変わらせます。安心・安全を第一に、お客様の理想をカタチにするお手伝いをいたします。</p>
+            </li>
+            <li class="business__item">
+                <h3 class="business__subTl">店舗の建築・リフォーム</h3>
+                <p class="business__txt txt">デザイン性と機能性を兼ね備えた空間づくりで、お店の魅力を最大限に引き出します。業態やコンセプトに合わせた提案を行い、集客力や働きやすさを向上。小規模な改修から大規模なリノベーションまで幅広く対応し、理想の店舗づくりをサポートします。お客様のビジョンをカタチにし、長く愛される空間を創り上げます。</p>
+            </li>
+        </ul>
+    </div>
+</section>
 
 <?php get_footer(); ?>
