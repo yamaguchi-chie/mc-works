1ba0396c10e2c76909944e8e1983b2be3f077e9a
https://git.phelix-system.com/ <masada@dadaworks.co.jp>
2025-03-10 11:07:31 +0900

    アニメーション

--- page-contact.php
+++ page-contact.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <section class="sub-contact">
     <div class="sub-contact__inner inner">
-        <div class="sub-contact__title">
+        <div class="sub-contact__title fade-in">
             <h1 class="page-title txt"><span>contact</span>お問い合わせ</h1>
         </div>
         <div class="sub-contact__body">
